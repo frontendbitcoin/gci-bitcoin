5617803d31a3980f2c21b2228d8fce5227fec552
frontendbitcoin <jackjenkins123@protonmail.com>
2021-11-24 19:00:28 +1100

    Finished core calculator UI

--- components/atoms/input-money.tsx
+++ components/atoms/input-money.tsx
@@ -1,34 +1,35 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+import { FC } from "react";
 import Cleave from "cleave.js/react";
 
-const InputMoney = () => (
+interface InputMoneyProps {
+  value: string;
+  // eslint-disable-next-line no-unused-vars
+  onChange: (e) => any;
+}
+
+const InputMoney: FC<InputMoneyProps> = ({ value, onChange }) => (
   <div className="w-full text-left max-w-4xl">
     <label htmlFor="price">
-      <span className="block text-sm font-medium text-gray-700">
-        USD Value of Bitcoin
+      <span className="block  font-medium text-gray-700">
+        Value of Bitcoin Holdings
       </span>
-      <div className="mt-1 relative rounded-md shadow-sm">
+      <div className="mt-4 relative rounded-md  shadow-sm">
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-          <span className="text-gray-500 sm:text-sm">$</span>
+          <span className="text-gray-500 text-lg">$</span>
         </div>
         <Cleave
           type="text"
           name="price"
           id="price"
-          className="my-2 p-5 pl-8 pr-8 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full  sm:text-sm border-gray-300 rounded-md"
+          className="border border-gray-300 my-2 text-lg tracking-wide p-5 pl-8 pr-8 focus:outline-none focus:ring-1 focus:ring-green-500 focus:border-green-500 block w-full border-gray-300 rounded-md"
           placeholder="0.00"
           aria-describedby="price-currency"
           options={{ numeral: true, numeralThousandsGroupStyle: "thousand" }}
+          value={value}
+          onChange={onChange}
         />
-        <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-          <span className="text-gray-500 sm:text-sm" id="price-currency">
-            USD
-          </span>
-        </div>
       </div>
-      <p className="mt-2 text-sm text-gray-500" id="email-description">
-        We do not send or store this information anywhere
-      </p>
     </label>
   </div>
 );
--- components/organisms/calculator.tsx
+++ components/organisms/calculator.tsx
@@ -1,29 +1,63 @@
+import { useState } from "react";
+import formatThousands from "format-thousands";
 import Icon from "components/atoms/icon";
 import InputMoney from "components/atoms/input-money";
 import { CalculatorIcon } from "@heroicons/react/outline";
 
-const Calculator = () => (
-  <div className="mt-24 relative overflow-hidden">
-    <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
-      <div className="flex justify-center">
-        <div className="max-w-4xl flex items-center flex-col">
-          <Icon>
-            <CalculatorIcon className="h-6 w-6 text-white" aria-hidden="true" />
-          </Icon>
-          <h2 className="mt-6 text-base font-semibold tracking-wider text-green-600 uppercase">
-            Calculator
-          </h2>
-          <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-            To make $X Bitcoin carbon neutral, you need to invest $Y per year in
-            clean energy Bitcoin mining
-          </p>
+const annualisedCleanEnergyBTCCalculator = (value: string) =>
+  String((Number(value) * 0.12).toFixed(2));
+
+const Calculator = () => {
+  const DEFAULT_USD_VALUE = "1000";
+  const DEFAULT_USD_VALUE_GCI =
+    annualisedCleanEnergyBTCCalculator(DEFAULT_USD_VALUE);
+  const [usdValueBTC, setUsdValueBTC] = useState(DEFAULT_USD_VALUE);
+  const [usdValueAnnualGCI, setUsdValueAnnualGCI] = useState(
+    DEFAULT_USD_VALUE_GCI
+  );
+
+  const onValueChange = (e: any) => {
+    const value = e.target.rawValue;
+    console.log("wayo", { value });
+    setUsdValueBTC(value);
+    setUsdValueAnnualGCI(annualisedCleanEnergyBTCCalculator(value));
+  };
+  return (
+    <div className="mt-24 relative overflow-hidden">
+      <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
+        <div className="flex justify-center">
+          <div className="max-w-4xl flex items-center flex-col">
+            <Icon>
+              <CalculatorIcon
+                className="h-6 w-6 text-white"
+                aria-hidden="true"
+              />
+            </Icon>
+            <h2 className="mt-6 text-base font-semibold tracking-wider text-green-600 uppercase">
+              Calculator
+            </h2>
+            <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+              <span className="inline">
+                To make{" "}
+                <span className="inline text-yellow-600">
+                  ${formatThousands(usdValueBTC, { separator: "," })} Bitcoin
+                  carbon neutral
+                </span>{" "}
+                , you need to invest{" "}
+              </span>
+              <span className="inline text-green-600">
+                ${formatThousands(usdValueAnnualGCI, { separator: "," })} per
+                year{" "}
+              </span>
+              in clean energy Bitcoin mining
+            </p>
+          </div>
+        </div>
+        <div className="mt-8 flex flex-1 justify-center ">
+          <InputMoney value={usdValueBTC} onChange={onValueChange} />
         </div>
       </div>
-      <div className="mt-8 flex flex-1 justify-center ">
-        <InputMoney />
-      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Calculator;
