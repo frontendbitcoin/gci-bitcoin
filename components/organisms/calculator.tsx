b248fd6a93433bc03704b06f2e4e744319f4efce
frontendbitcoin <jackjenkins123@protonmail.com>
2021-11-27 11:50:07 +1100

    Created a clean card wrapper for the calculator

--- components/organisms/calculator.tsx
+++ components/organisms/calculator.tsx
@@ -23,47 +23,53 @@ const Calculator = () => {
     setUsdValueAnnualGCI(annualisedCleanEnergyBTCCalculator(value));
   };
   return (
-    <div className="mt-24 relative overflow-hidden">
-      <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
-        <div className="flex justify-center">
-          <div className="max-w-4xl flex items-center flex-col">
-            <Icon>
-              <CalculatorIcon
-                className="h-6 w-6 text-white"
-                aria-hidden="true"
-              />
-            </Icon>
-            <h2 className="mt-6 text-base font-semibold tracking-wider text-green-600 uppercase">
-              Calculator
-            </h2>
-            <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-              <span className="inline">
-                To make{" "}
-                <span className="inline text-yellow-600">
-                  ${formatThousands(usdValueBTC, { separator: "," })} Bitcoin
-                  carbon neutral
-                </span>{" "}
-                , you need to invest{" "}
-              </span>
-              <span className="inline text-green-600">
-                ${formatThousands(usdValueAnnualGCI, { separator: "," })} per
-                year{" "}
-              </span>
-              in clean energy Bitcoin mining
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-col items-center">
-          <div className="max-w-4xl w-full text-left">
-            <div className="mt-8">
-              <InputMoney value={usdValueBTC} onChange={onValueChange} />
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+        <div className="rounded-lg border border-gray-100 shadow-lg overflow-hidden">
+          <div className="my-12 relative overflow-hidden">
+            <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
+              <div className="flex justify-center">
+                <div className="max-w-4xl flex items-center flex-col">
+                  <Icon>
+                    <CalculatorIcon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
+                    />
+                  </Icon>
+                  <h2 className="mt-6 text-base font-semibold tracking-wider text-green-600 uppercase">
+                    Calculator
+                  </h2>
+                  <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+                    <span className="inline">
+                      To make{" "}
+                      <span className="inline text-yellow-600">
+                        ${formatThousands(usdValueBTC, { separator: "," })}{" "}
+                        Bitcoin carbon neutral
+                      </span>{" "}
+                      , you need to invest{" "}
+                    </span>
+                    <span className="inline text-green-600">
+                      ${formatThousands(usdValueAnnualGCI, { separator: "," })}{" "}
+                      per year{" "}
+                    </span>
+                    in clean energy Bitcoin mining
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col items-center">
+                <div className="max-w-4xl w-full text-left">
+                  <div className="mt-8">
+                    <InputMoney value={usdValueBTC} onChange={onValueChange} />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-8 flex flex-col items-center">
+              <div className="max-w-4xl w-full">
+                <CalculatorOutputs value={usdValueAnnualGCI} />
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="mt-8 flex flex-col items-center">
-        <div className="max-w-4xl w-full">
-          <CalculatorOutputs value={usdValueAnnualGCI} />
         </div>
       </div>
     </div>
